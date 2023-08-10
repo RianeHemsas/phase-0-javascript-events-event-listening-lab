e5df652fb1fe1ef004b2b32f0b698868e7d0aa70
Hemsas Riane <rayan.hemsas@gmail.com>
2023-08-10 08:48:39 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+  
 function addingEventListener() {
+let input = document.getElementById('button');
+input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
 }
+
+addingEventListener();
